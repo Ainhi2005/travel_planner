29c477b6dd49e66d7c5561f773e817e929722893
Ainhi2005 <your_email@example.com>
2026-05-30 13:40:43 +0700

    feat: implement AuthLocalDataSource and update dependencies for data persistence

--- lib/features/auth/data/datasources/auth_local_data_source.dart
+++ lib/features/auth/data/datasources/auth_local_data_source.dart
@@ -1,20 +1,26 @@
+//Lưu trữ, Đọc ra và Xóa thông tin người dùng ngay trên thiết bị di động.
 import 'dart:convert';
+
 import 'package:shared_preferences/shared_preferences.dart';
-import '../models/user_ui_model.dart';
+import 'package:travel_planner/features/auth/data/models/user_ui_model.dart';
 
 abstract class AuthLocalDataSource {
   Future<void> saveUser(UserModel user);
   Future<UserModel?> getUser();
   Future<void> clearUser();
+  // xử lý phần nhớ mật khẩu
+  //....
 }
 
 class AuthLocalDataSourceImpl implements AuthLocalDataSource {
   static const _userKey = 'cached_user';
-
   @override
   Future<void> saveUser(UserModel user) async {
-    final prefs = await SharedPreferences.getInstance();
+    final prefs = await SharedPreferences.getInstance(); // khởi tạo
     final jsonString = jsonEncode(user.toJson());
+    //.tojson() : từ model -> Map<String, dymic>
+    // jsonEncode : từ Map<String, dymic> -> String dạng json
+    // setString : lưu string vào bộ nhớ
     await prefs.setString(_userKey, jsonString);
   }
 
@@ -22,10 +28,15 @@ class AuthLocalDataSourceImpl implements AuthLocalDataSource {
   Future<UserModel?> getUser() async {
     final prefs = await SharedPreferences.getInstance();
     final jsonString = prefs.getString(_userKey);
-    if (jsonString != null) {
-      return UserModel.fromJson(jsonDecode(jsonString));
+    //.getstring() : lấy chuỗi từ bộ nhớ
+    // lúc này jsonString có dạng "{}" (string)
+    // null: không có ai đăng nhập
+    if (jsonString == null) {
+      return null;
     }
-    return null;
+    return UserModel.fromJson(jsonDecode(jsonString));
+    //jsonDecode : từ String dạng json -> Map<String, dymic>
+    //fromJson : từ Map<String, dymic> -> model để trả về dạng model
   }
 
   @override
